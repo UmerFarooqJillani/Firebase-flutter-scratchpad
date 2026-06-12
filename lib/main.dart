ba0616d07ee697fd490f8232a8c21e56bf07563b
Umer Farooq Jillani <umerfarooqjillani92@gmail.com>
2026-06-12 15:27:51 +0500

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:firebase_scratchpad/services/firebase/firebase_initializer.dart';
 // import 'package:firebase_scratchpad/features/authentication/email_password_auth/presentation/signup_screen.dart';
 // import 'package:firebase_scratchpad/features/authentication/phone_otp_auth/presentation/phone_login_screen.dart';
-import 'package:firebase_scratchpad/features/firestore_learning/presentation/firestore_crud_screen.dart';
+import 'package:firebase_scratchpad/features/firestore_learning/crud/presentation/firestore_crud_screen.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
